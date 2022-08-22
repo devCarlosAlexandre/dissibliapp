64390f6c6006aa8184bc3a68eb814f1e46660a41
devCarlosALexandre <carloalexandresousa@gmail.com>
2022-08-22 20:43:40 -0300

    ok

---
+++ lib/widget/dimisble_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Teste extends StatefulWidget {
+  const Teste({Key? key}) : super(key: key);
+
+  @override
+  State<Teste> createState() => _TesteState();
+}
+
+class _TesteState extends State<Teste> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Deck");
+  }
+}
